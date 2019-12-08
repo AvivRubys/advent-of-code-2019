76161a2a8635385e337f4ebe914bc2b2f0822a98
Aviv Rozenboim <aviv@soluto.com>
2019-12-08 15:24:50 +0200

    Day 8 part 2

--- 8/index.js
+++ 8/index.js
@@ -1,19 +1,28 @@
 const fs = require('fs');
+const os = require('os');
 const R = require('ramda');
 
 const WIDTH = 25,
     HEIGHT = 6;
 
-const input = fs
-    .readFileSync('./input.txt')
-    .toString()
-    .split('');
-const result = R.pipe(
-    R.splitEvery(WIDTH * HEIGHT),
+const layers = R.pipe(R.split(''), R.splitEvery(WIDTH * HEIGHT))(fs.readFileSync('./input.txt').toString());
+
+// part 1
+R.pipe(
     R.map(R.countBy(x => x)),
     R.reduce(
         R.minBy(layer => layer['0']),
         {'0': Infinity}
     ),
     val => console.log(val['1'] * val['2'])
-)(input);
+)(layers);
+
+// part 2
+const zipMany = list => R.reduce(R.pipe(R.zip, R.map(R.flatten)), R.head(list), R.tail(list));
+R.pipe(
+    zipMany,
+    R.map(R.find(a => a !== '2')),
+    R.map(x => (x === '0' ? ' ' : '#')),
+    R.splitEvery(WIDTH),
+    R.forEach(R.pipe(R.join(' '), console.log))
+)(layers);
